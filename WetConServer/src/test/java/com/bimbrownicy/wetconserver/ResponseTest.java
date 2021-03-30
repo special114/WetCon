736f9a9720bc4a96f0500d44b78bc2c86787c003
Rafał Surdej <01143612@pw.edu.pl>
2021-03-30 22:58:47 +0200

    Test written

---
+++ WetConServer/src/test/java/com/bimbrownicy/wetconserver/ResponseTest.java
@@ -0,0 +1,16 @@
+package com.bimbrownicy.wetconserver;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ResponseTest {
+
+    @Test
+    public void returnTest() {
+        Response response = new Response();
+        assertEquals("Hello World!", response.getText());
+    }
+
+}
